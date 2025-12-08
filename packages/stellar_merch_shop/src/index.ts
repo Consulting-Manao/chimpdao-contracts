afb0cc2f3f275484ced3dd844062c2206fdc944d
Tupui <23188539+tupui@users.noreply.github.com>
2025-12-08 15:32:30 +0100

    Add packages to git

---
+++ packages/stellar_merch_shop/src/index.ts
@@ -0,0 +1,228 @@
+import { Buffer } from "buffer";
+import { Address } from '@stellar/stellar-sdk';
+import {
+  AssembledTransaction,
+  Client as ContractClient,
+  ClientOptions as ContractClientOptions,
+  MethodOptions,
+  Result,
+  Spec as ContractSpec,
+} from '@stellar/stellar-sdk/contract';
+import type {
+  u32,
+  i32,
+  u64,
+  i64,
+  u128,
+  i128,
+  u256,
+  i256,
+  AssembledTransactionOptions,
+  Option,
+  Typepoint,
+  Duration,
+} from '@stellar/stellar-sdk/contract';
+export * from '@stellar/stellar-sdk'
+export * as contract from '@stellar/stellar-sdk/contract'
+export * as rpc from '@stellar/stellar-sdk/rpc'
+
+if (typeof window !== 'undefined') {
+  //@ts-ignore Buffer exists
+  window.Buffer = window.Buffer || Buffer;
+}
+
+
+
+
+export type DataKey = {tag: "Admin", values: void} | {tag: "Nonce", values: void};
+
+export type NFTStorageKey = {tag: "ChipNonce", values: readonly [Buffer]} | {tag: "Owner", values: readonly [Buffer]} | {tag: "Balance", values: readonly [string]} | {tag: "Approval", values: readonly [u32]} | {tag: "ApprovalForAll", values: readonly [string, string]} | {tag: "Name", values: void} | {tag: "Symbol", values: void} | {tag: "URI", values: void};
+
+export const NonFungibleTokenError = {
+  /**
+   * Indicates a non-existent `token_id`.
+   */
+  200: {message:"NonExistentToken"},
+  /**
+   * Indicates an error related to the ownership over a particular token.
+   * Used in transfers.
+   */
+  201: {message:"IncorrectOwner"},
+  /**
+   * Indicates a failure with the `operator`s approval. Used in transfers.
+   */
+  202: {message:"InsufficientApproval"},
+  /**
+   * Indicates a failure with the `approver` of a token to be approved. Used
+   * in approvals.
+   */
+  203: {message:"InvalidApprover"},
+  /**
+   * Indicates an invalid value for `live_until_ledger` when setting
+   * approvals.
+   */
+  204: {message:"InvalidLiveUntilLedger"},
+  /**
+   * Indicates overflow when adding two values
+   */
+  205: {message:"MathOverflow"},
+  /**
+   * Indicates all possible `token_id`s are already in use.
+   */
+  206: {message:"TokenIDsAreDepleted"},
+  /**
+   * Indicates an invalid amount to batch mint in `consecutive` extension.
+   */
+  207: {message:"InvalidAmount"},
+  /**
+   * Indicates the token does not exist in owner's list.
+   */
+  208: {message:"TokenNotFoundInOwnerList"},
+  /**
+   * Indicates the token does not exist in global list.
+   */
+  209: {message:"TokenNotFoundInGlobalList"},
+  /**
+   * Indicates the token was already minted.
+   */
+  210: {message:"TokenAlreadyMinted"},
+  /**
+   * Indicates the length of the base URI exceeds the maximum allowed.
+   */
+  211: {message:"BaseUriMaxLenExceeded"},
+  /**
+   * Indicates the royalty amount is higher than 10_000 (100%) basis points.
+   */
+  212: {message:"InvalidRoyaltyAmount"}
+}
+
+
+
+
+
+export interface Client {
+  /**
+   * Construct and simulate a mint transaction. Returns an `AssembledTransaction` object which will have a `result` field containing the result of the simulation. If this transaction changes contract state, you will need to call `signAndSend()` on the returned object.
+   */
+  mint: ({to, message, signature, recovery_id, nonce}: {to: string, message: Buffer, signature: Buffer, recovery_id: u32, nonce: u32}, options?: AssembledTransactionOptions<Buffer>) => Promise<AssembledTransaction<Buffer>>
+
+  /**
+   * Construct and simulate a balance transaction. Returns an `AssembledTransaction` object which will have a `result` field containing the result of the simulation. If this transaction changes contract state, you will need to call `signAndSend()` on the returned object.
+   */
+  balance: ({owner}: {owner: string}, options?: AssembledTransactionOptions<u32>) => Promise<AssembledTransaction<u32>>
+
+  /**
+   * Construct and simulate a owner_of transaction. Returns an `AssembledTransaction` object which will have a `result` field containing the result of the simulation. If this transaction changes contract state, you will need to call `signAndSend()` on the returned object.
+   */
+  owner_of: ({token_id}: {token_id: Buffer}, options?: AssembledTransactionOptions<string>) => Promise<AssembledTransaction<string>>
+
+  /**
+   * Construct and simulate a transfer transaction. Returns an `AssembledTransaction` object which will have a `result` field containing the result of the simulation. If this transaction changes contract state, you will need to call `signAndSend()` on the returned object.
+   */
+  transfer: ({from, to, token_id}: {from: string, to: string, token_id: Buffer}, options?: AssembledTransactionOptions<null>) => Promise<AssembledTransaction<null>>
+
+  /**
+   * Construct and simulate a transfer_from transaction. Returns an `AssembledTransaction` object which will have a `result` field containing the result of the simulation. If this transaction changes contract state, you will need to call `signAndSend()` on the returned object.
+   */
+  transfer_from: ({spender, from, to, token_id}: {spender: string, from: string, to: string, token_id: Buffer}, options?: AssembledTransactionOptions<null>) => Promise<AssembledTransaction<null>>
+
+  /**
+   * Construct and simulate a approve transaction. Returns an `AssembledTransaction` object which will have a `result` field containing the result of the simulation. If this transaction changes contract state, you will need to call `signAndSend()` on the returned object.
+   */
+  approve: ({approver, approved, token_id, live_until_ledger}: {approver: string, approved: string, token_id: Buffer, live_until_ledger: u32}, options?: AssembledTransactionOptions<null>) => Promise<AssembledTransaction<null>>
+
+  /**
+   * Construct and simulate a approve_for_all transaction. Returns an `AssembledTransaction` object which will have a `result` field containing the result of the simulation. If this transaction changes contract state, you will need to call `signAndSend()` on the returned object.
+   */
+  approve_for_all: ({owner, operator, live_until_ledger}: {owner: string, operator: string, live_until_ledger: u32}, options?: AssembledTransactionOptions<null>) => Promise<AssembledTransaction<null>>
+
+  /**
+   * Construct and simulate a get_approved transaction. Returns an `AssembledTransaction` object which will have a `result` field containing the result of the simulation. If this transaction changes contract state, you will need to call `signAndSend()` on the returned object.
+   */
+  get_approved: ({token_id}: {token_id: Buffer}, options?: AssembledTransactionOptions<Option<string>>) => Promise<AssembledTransaction<Option<string>>>
+
+  /**
+   * Construct and simulate a is_approved_for_all transaction. Returns an `AssembledTransaction` object which will have a `result` field containing the result of the simulation. If this transaction changes contract state, you will need to call `signAndSend()` on the returned object.
+   */
+  is_approved_for_all: ({owner, operator}: {owner: string, operator: string}, options?: AssembledTransactionOptions<boolean>) => Promise<AssembledTransaction<boolean>>
+
+  /**
+   * Construct and simulate a get_nonce transaction. Returns an `AssembledTransaction` object which will have a `result` field containing the result of the simulation. If this transaction changes contract state, you will need to call `signAndSend()` on the returned object.
+   */
+  get_nonce: ({token_id}: {token_id: Buffer}, options?: AssembledTransactionOptions<u32>) => Promise<AssembledTransaction<u32>>
+
+  /**
+   * Construct and simulate a name transaction. Returns an `AssembledTransaction` object which will have a `result` field containing the result of the simulation. If this transaction changes contract state, you will need to call `signAndSend()` on the returned object.
+   */
+  name: (options?: AssembledTransactionOptions<string>) => Promise<AssembledTransaction<string>>
+
+  /**
+   * Construct and simulate a symbol transaction. Returns an `AssembledTransaction` object which will have a `result` field containing the result of the simulation. If this transaction changes contract state, you will need to call `signAndSend()` on the returned object.
+   */
+  symbol: (options?: AssembledTransactionOptions<string>) => Promise<AssembledTransaction<string>>
+
+  /**
+   * Construct and simulate a token_uri transaction. Returns an `AssembledTransaction` object which will have a `result` field containing the result of the simulation. If this transaction changes contract state, you will need to call `signAndSend()` on the returned object.
+   */
+  token_uri: ({token_id}: {token_id: Buffer}, options?: AssembledTransactionOptions<string>) => Promise<AssembledTransaction<string>>
+
+}
+export class Client extends ContractClient {
+  static async deploy<T = Client>(
+        /** Constructor/Initialization Args for the contract's `__constructor` method */
+        {admin, name, symbol, uri}: {admin: string, name: string, symbol: string, uri: string},
+    /** Options for initializing a Client as well as for calling a method, with extras specific to deploying. */
+    options: MethodOptions &
+      Omit<ContractClientOptions, "contractId"> & {
+        /** The hash of the Wasm blob, which must already be installed on-chain. */
+        wasmHash: Buffer | string;
+        /** Salt used to generate the contract's ID. Passed through to {@link Operation.createCustomContract}. Default: random. */
+        salt?: Buffer | Uint8Array;
+        /** The format used to decode `wasmHash`, if it's provided as a string. */
+        format?: "hex" | "base64";
+      }
+  ): Promise<AssembledTransaction<T>> {
+    return ContractClient.deploy({admin, name, symbol, uri}, options)
+  }
+  constructor(public readonly options: ContractClientOptions) {
+    super(
+      new ContractSpec([ "AAAAAgAAAAAAAAAAAAAAB0RhdGFLZXkAAAAAAgAAAAAAAAAAAAAABUFkbWluAAAAAAAAAAAAAAAAAAAFTm9uY2UAAAA=",
+        "AAAAAgAAAAAAAAAAAAAADU5GVFN0b3JhZ2VLZXkAAAAAAAAIAAAAAQAAAAAAAAAJQ2hpcE5vbmNlAAAAAAAAAQAAA+4AAABBAAAAAQAAAAAAAAAFT3duZXIAAAAAAAABAAAD7gAAAEEAAAABAAAAAAAAAAdCYWxhbmNlAAAAAAEAAAATAAAAAQAAAAAAAAAIQXBwcm92YWwAAAABAAAABAAAAAEAAAAAAAAADkFwcHJvdmFsRm9yQWxsAAAAAAACAAAAEwAAABMAAAAAAAAAAAAAAAROYW1lAAAAAAAAAAAAAAAGU3ltYm9sAAAAAAAAAAAAAAAAAANVUkkA",
+        "AAAAAAAAAAAAAAANX19jb25zdHJ1Y3RvcgAAAAAAAAQAAAAAAAAABWFkbWluAAAAAAAAEwAAAAAAAAAEbmFtZQAAABAAAAAAAAAABnN5bWJvbAAAAAAAEAAAAAAAAAADdXJpAAAAABAAAAAA",
+        "AAAAAAAAAAAAAAAEbWludAAAAAUAAAAAAAAAAnRvAAAAAAATAAAAAAAAAAdtZXNzYWdlAAAAAA4AAAAAAAAACXNpZ25hdHVyZQAAAAAAA+4AAABAAAAAAAAAAAtyZWNvdmVyeV9pZAAAAAAEAAAAAAAAAAVub25jZQAAAAAAAAQAAAABAAAD7gAAAEE=",
+        "AAAAAAAAAAAAAAAHYmFsYW5jZQAAAAABAAAAAAAAAAVvd25lcgAAAAAAABMAAAABAAAABA==",
+        "AAAAAAAAAAAAAAAIb3duZXJfb2YAAAABAAAAAAAAAAh0b2tlbl9pZAAAA+4AAABBAAAAAQAAABM=",
+        "AAAAAAAAAAAAAAAIdHJhbnNmZXIAAAADAAAAAAAAAARmcm9tAAAAEwAAAAAAAAACdG8AAAAAABMAAAAAAAAACHRva2VuX2lkAAAD7gAAAEEAAAAA",
+        "AAAAAAAAAAAAAAANdHJhbnNmZXJfZnJvbQAAAAAAAAQAAAAAAAAAB3NwZW5kZXIAAAAAEwAAAAAAAAAEZnJvbQAAABMAAAAAAAAAAnRvAAAAAAATAAAAAAAAAAh0b2tlbl9pZAAAA+4AAABBAAAAAA==",
+        "AAAAAAAAAAAAAAAHYXBwcm92ZQAAAAAEAAAAAAAAAAhhcHByb3ZlcgAAABMAAAAAAAAACGFwcHJvdmVkAAAAEwAAAAAAAAAIdG9rZW5faWQAAAPuAAAAQQAAAAAAAAARbGl2ZV91bnRpbF9sZWRnZXIAAAAAAAAEAAAAAA==",
+        "AAAAAAAAAAAAAAAPYXBwcm92ZV9mb3JfYWxsAAAAAAMAAAAAAAAABW93bmVyAAAAAAAAEwAAAAAAAAAIb3BlcmF0b3IAAAATAAAAAAAAABFsaXZlX3VudGlsX2xlZGdlcgAAAAAAAAQAAAAA",
+        "AAAAAAAAAAAAAAAMZ2V0X2FwcHJvdmVkAAAAAQAAAAAAAAAIdG9rZW5faWQAAAPuAAAAQQAAAAEAAAPoAAAAEw==",
+        "AAAAAAAAAAAAAAATaXNfYXBwcm92ZWRfZm9yX2FsbAAAAAACAAAAAAAAAAVvd25lcgAAAAAAABMAAAAAAAAACG9wZXJhdG9yAAAAEwAAAAEAAAAB",
+        "AAAAAAAAAAAAAAAJZ2V0X25vbmNlAAAAAAAAAQAAAAAAAAAIdG9rZW5faWQAAAPuAAAAQQAAAAEAAAAE",
+        "AAAAAAAAAAAAAAAEbmFtZQAAAAAAAAABAAAAEA==",
+        "AAAAAAAAAAAAAAAGc3ltYm9sAAAAAAAAAAAAAQAAABA=",
+        "AAAAAAAAAAAAAAAJdG9rZW5fdXJpAAAAAAAAAQAAAAAAAAAIdG9rZW5faWQAAAPuAAAAQQAAAAEAAAAQ",
+        "AAAABAAAAAAAAAAAAAAAFU5vbkZ1bmdpYmxlVG9rZW5FcnJvcgAAAAAAAA0AAAAkSW5kaWNhdGVzIGEgbm9uLWV4aXN0ZW50IGB0b2tlbl9pZGAuAAAAEE5vbkV4aXN0ZW50VG9rZW4AAADIAAAAV0luZGljYXRlcyBhbiBlcnJvciByZWxhdGVkIHRvIHRoZSBvd25lcnNoaXAgb3ZlciBhIHBhcnRpY3VsYXIgdG9rZW4uClVzZWQgaW4gdHJhbnNmZXJzLgAAAAAOSW5jb3JyZWN0T3duZXIAAAAAAMkAAABFSW5kaWNhdGVzIGEgZmFpbHVyZSB3aXRoIHRoZSBgb3BlcmF0b3JgcyBhcHByb3ZhbC4gVXNlZCBpbiB0cmFuc2ZlcnMuAAAAAAAAFEluc3VmZmljaWVudEFwcHJvdmFsAAAAygAAAFVJbmRpY2F0ZXMgYSBmYWlsdXJlIHdpdGggdGhlIGBhcHByb3ZlcmAgb2YgYSB0b2tlbiB0byBiZSBhcHByb3ZlZC4gVXNlZAppbiBhcHByb3ZhbHMuAAAAAAAAD0ludmFsaWRBcHByb3ZlcgAAAADLAAAASkluZGljYXRlcyBhbiBpbnZhbGlkIHZhbHVlIGZvciBgbGl2ZV91bnRpbF9sZWRnZXJgIHdoZW4gc2V0dGluZwphcHByb3ZhbHMuAAAAAAAWSW52YWxpZExpdmVVbnRpbExlZGdlcgAAAAAAzAAAAClJbmRpY2F0ZXMgb3ZlcmZsb3cgd2hlbiBhZGRpbmcgdHdvIHZhbHVlcwAAAAAAAAxNYXRoT3ZlcmZsb3cAAADNAAAANkluZGljYXRlcyBhbGwgcG9zc2libGUgYHRva2VuX2lkYHMgYXJlIGFscmVhZHkgaW4gdXNlLgAAAAAAE1Rva2VuSURzQXJlRGVwbGV0ZWQAAAAAzgAAAEVJbmRpY2F0ZXMgYW4gaW52YWxpZCBhbW91bnQgdG8gYmF0Y2ggbWludCBpbiBgY29uc2VjdXRpdmVgIGV4dGVuc2lvbi4AAAAAAAANSW52YWxpZEFtb3VudAAAAAAAAM8AAAAzSW5kaWNhdGVzIHRoZSB0b2tlbiBkb2VzIG5vdCBleGlzdCBpbiBvd25lcidzIGxpc3QuAAAAABhUb2tlbk5vdEZvdW5kSW5Pd25lckxpc3QAAADQAAAAMkluZGljYXRlcyB0aGUgdG9rZW4gZG9lcyBub3QgZXhpc3QgaW4gZ2xvYmFsIGxpc3QuAAAAAAAZVG9rZW5Ob3RGb3VuZEluR2xvYmFsTGlzdAAAAAAAANEAAAAnSW5kaWNhdGVzIHRoZSB0b2tlbiB3YXMgYWxyZWFkeSBtaW50ZWQuAAAAABJUb2tlbkFscmVhZHlNaW50ZWQAAAAAANIAAABBSW5kaWNhdGVzIHRoZSBsZW5ndGggb2YgdGhlIGJhc2UgVVJJIGV4Y2VlZHMgdGhlIG1heGltdW0gYWxsb3dlZC4AAAAAAAAVQmFzZVVyaU1heExlbkV4Y2VlZGVkAAAAAAAA0wAAAEdJbmRpY2F0ZXMgdGhlIHJveWFsdHkgYW1vdW50IGlzIGhpZ2hlciB0aGFuIDEwXzAwMCAoMTAwJSkgYmFzaXMgcG9pbnRzLgAAAAAUSW52YWxpZFJveWFsdHlBbW91bnQAAADU",
+        "AAAABQAAAAAAAAAAAAAACFRyYW5zZmVyAAAAAQAAAAh0cmFuc2ZlcgAAAAMAAAAAAAAABGZyb20AAAATAAAAAQAAAAAAAAACdG8AAAAAABMAAAABAAAAAAAAAAh0b2tlbl9pZAAAAAQAAAAAAAAAAg==",
+        "AAAABQAAAAAAAAAAAAAAB0FwcHJvdmUAAAAAAQAAAAdhcHByb3ZlAAAAAAQAAAAAAAAACGFwcHJvdmVyAAAAEwAAAAEAAAAAAAAACHRva2VuX2lkAAAABAAAAAEAAAAAAAAACGFwcHJvdmVkAAAAEwAAAAAAAAAAAAAAEWxpdmVfdW50aWxfbGVkZ2VyAAAAAAAABAAAAAAAAAAC",
+        "AAAABQAAAAAAAAAAAAAADUFwcHJvdmVGb3JBbGwAAAAAAAABAAAAD2FwcHJvdmVfZm9yX2FsbAAAAAADAAAAAAAAAAVvd25lcgAAAAAAABMAAAABAAAAAAAAAAhvcGVyYXRvcgAAABMAAAAAAAAAAAAAABFsaXZlX3VudGlsX2xlZGdlcgAAAAAAAAQAAAAAAAAAAg==",
+        "AAAABQAAAAAAAAAAAAAABE1pbnQAAAABAAAABG1pbnQAAAACAAAAAAAAAAJ0bwAAAAAAEwAAAAEAAAAAAAAACHRva2VuX2lkAAAD7gAAAEEAAAAAAAAAAg==" ]),
+      options
+    )
+  }
+  public readonly fromJSON = {
+    mint: this.txFromJSON<Buffer>,
+        balance: this.txFromJSON<u32>,
+        owner_of: this.txFromJSON<string>,
+        transfer: this.txFromJSON<null>,
+        transfer_from: this.txFromJSON<null>,
+        approve: this.txFromJSON<null>,
+        approve_for_all: this.txFromJSON<null>,
+        get_approved: this.txFromJSON<Option<string>>,
+        is_approved_for_all: this.txFromJSON<boolean>,
+        get_nonce: this.txFromJSON<u32>,
+        name: this.txFromJSON<string>,
+        symbol: this.txFromJSON<string>,
+        token_uri: this.txFromJSON<string>
+  }
+}
