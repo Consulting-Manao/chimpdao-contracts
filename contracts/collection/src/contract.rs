cc355f8971763e934f1c7f87bf97df9a7cff9a38
Tupui <23188539+tupui@users.noreply.github.com>
2026-02-09 15:33:35 +0100

    Basis around collections

--- contracts/collection/src/contract.rs
+++ contracts/collection/src/contract.rs
@@ -1,6 +1,6 @@
 //! NFC Collection
 
-use soroban_sdk::{contractimpl, contracttype, panic_with_error, Address, Bytes, BytesN, Env, String};
+use soroban_sdk::{contractimpl, contracttype, panic_with_error, Address, Bytes, BytesN, Env, String, Vec};
 use soroban_sdk::xdr::ToXdr;
 use crate::{errors, events, Collection, CollectionArgs, CollectionClient, CollectionTrait};
 
@@ -12,6 +12,8 @@ pub enum DataKey {
 #[contracttype]
 pub enum CollectionKey {
     NFTContract,
+    Collections,
+    Collectibles(Address),
 }
 
 
@@ -27,9 +29,31 @@ impl CollectionTrait for Collection {
         admin.require_auth();
 
         e.deployer().update_current_contract_wasm(wasm_hash.clone());
-
-        events::Upgrade { admin, wasm_hash: wasm_hash.into() }.publish(e);
     }
 
+    fn create_collection(e: &Env, wasm_hash: BytesN<32>, name: String, symbol: String, uri: String, max_tokens: u32) {
+        let admin: Address = e.storage().instance().get(&DataKey::Admin).unwrap();
+        admin.require_auth();
+
+        let salt: BytesN<32> = e.crypto().sha256(&symbol.to_bytes()).into();
+        let deployer = e.deployer().with_current_contract(salt);
+        let contract_address = deployer.deploy_v2(wasm_hash, (name, symbol, uri, max_tokens));
+
+        let mut collections: Vec<Address> = e.storage().instance().get(&CollectionKey::Collections).unwrap_or(Vec::new(&e));
+        collections.push_back(contract_address.clone());
+        e.storage().instance().set(&CollectionKey::Collections, &contract_address);
+    }
+
+    fn assign_collectible(e: &Env, collection: Address, to: Address, token_id: u32) {
+        collection.require_auth();
+
+        let mut collectibles: Vec<(Address, u32)> = e.storage().instance().get(&CollectionKey::Collectibles(to.clone())).unwrap_or(Vec::new(&e));
+        collectibles.push_back((collection, token_id));
+        e.storage().instance().set(&CollectionKey::Collectibles(to), &collectibles);
+    }
+
+    fn collectibles(e: &Env, from: Address) -> Vec<(Address, u32)> {
+        e.storage().instance().get(&CollectionKey::Collectibles(from.clone())).unwrap_or(Vec::new(&e))
+    }
 
 }
--- contracts/collection/src/lib.rs
+++ contracts/collection/src/lib.rs
@@ -1,7 +1,7 @@
 #![no_std]
 #![allow(dead_code)]
 
-use soroban_sdk::{contract, contractmeta, Env, Address, String, BytesN, Bytes};
+use soroban_sdk::{contract, contractmeta, Env, Address, String, BytesN, Bytes, Vec};
 
 contractmeta!(key = "Description", val = "ChimpDAO Collection");
 
@@ -20,5 +20,11 @@ pub trait CollectionTrait {
     fn __constructor(e: &Env, admin: Address);
 
     fn upgrade(e: &Env, wasm_hash: BytesN<32>);
+
+    fn create_collection(e: &Env, wasm_hash: BytesN<32>, name: String, symbol: String, uri: String, max_tokens: u32);
+
+    fn assign_collectible(e: &Env, collection: Address, to: Address, token_id: u32);
+
+    fn collectibles(e: &Env, from: Address) -> Vec<(Address, u32)>;
 
 }
